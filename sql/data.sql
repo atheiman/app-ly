b9a5844c6ca76a5dfc326c8f02a6b85bce242b13
Austin Heiman <atheimanksu@gmail.com>
2014-03-15 03:50:10 +0000

    sql work, added resume, work_history, jobs, applicants tables

--- sql/data.sql
+++ sql/data.sql
@@ -1,8 +1,35 @@
 use app_ly;
 
-insert into jobs ( title , field , page , expiration_date )
-values
-( 'Senior Account Representative' , 'Sales Marketing' , 'senior_account_rep.php' , 2014-06-01 ) ,
-( 'Junior Account Representative' , 'Sales Marketing' , 'junior_account_rep.php' , 2014-06-15 ) ,
-( 'Systems Engineer' , 'Computer Science Engineering Technology' , 'systems_engineer.php' , 2014-07-22) ;
+delete from jobs ;
+delete from resumes ;
+delete from work_history ;
+delete from applicants ;
 
+insert into jobs ( title , tags , description , city , country , posted , expiration , openings )
+values
+( 'Systems Engineer' , 'technology computer science engineering' , 'If you are an experienced Linux Systems Engineer; The Intersect Group has an exciting opportunity for you! We create unparalleled value for our clients and candidates by combining industry-leading consulting capabilities with comprehensive staffing and recruitment services. One of our clients is seeking a System Administrator Generalist with a strong website support background that can provide technical expertise for database administrators, applications developers, and application owners. If that sounds like you and your ready to take your career to the next level; let us help you succeed at the intersection of work and life!' , 'Kansas City' , 'United States' , '2014-03-12' , '2014-05-15' , 2 ) ,
+( 'DevOps Engineer' , 'technology computer science engineering' , 'The Data DevOps Engineer plans and conducts activities concerned with ensuring the performance and reliability of our software applications.  This includes monitoring, validating changes, gathering and reporting metrics, testing, incident management, change management, etc.  Able to install, debug and support various environments that support CSIdentity applications.  The Data Engineering Support Engineer is expected to complete tasks independently and with minimum support from management staff.  S/he has a strong ability to grasp new technologies and acquire new skills through independent study and interaction with other team members.' , 'Atlanta' , 'United States' , '2014-03-10' , '2014-06-01' , 1 ) 
+;
+
+insert into applicants ( email , firstname , lastname , city , country , phone )
+values
+( 'spongebob@pineapple.com' , 'Spongebob' , 'Squarepants' , 'Dallas' , 'United States' , '934-481-6095' ) ,
+( 'patrick@rock.com' , 'Patrick' , 'Star' , 'Ontario' , 'Canada' , '724-589-3759' ) ,
+( 'squidward@easterisland.net' , 'Squidward' , 'Tentacles' , 'Seattle' , 'United States' , '485-592-6703' ) ,
+( 'sandy@dome.org' , 'Sandy' , 'Cheeks' , 'Houston' , 'United States' , '452-547-9703' )
+;
+
+insert into resumes ( email , resume )
+values
+( 'spongebob@pineapple.com' , 'Spongebob Squarepants spongebob@pineapple.com Fry Cook Krusty Krab Cooked delicious Krabby Patties for hungry customers. References available on request.' ) ,
+( 'patrick@rock.com' , 'Patrick Star patrick@rock.com Professional sleeper. Spent many hours sleeping.' ) ,
+( 'squidward@easterisland.net' , 'Squidward Tentacles squidward@easterisland.net Cashier Krusty Krab Calmed impatient customers requesting Krabby Patties. Responsible for handling large quantities of cash' ) ,
+( 'sandy@dome.org' , 'Sandy Cheeks sandy@dome.org Rancher Raised and sold cattle profitably for 8 years.' )
+;
+
+insert into work_history ( email , title , employer , start_date , end_date )
+values
+( 'spongebob@pineapple.com' , 'Frycook' , 'Weenie Hut General' , '2001-07-15' , '2004-01-01' ) ,
+( 'spongebob@pineapple.com' , 'Frycook' , 'Krusty Krab' , '1999-05-01' ) ,
+
+;
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,16 +1,53 @@
 use app_ly;
 -- drop old tables
 drop table jobs;
+drop table resumes;
+drop table work_history;
+drop table applicants;
 
 -- create new tables
 create table jobs
 (
 id int not null auto_increment,
 title varchar(255) not null,
-field varchar(255) not null,
-page varchar(255) not null,
-expiration_date date not null,
+tags varchar(255) not null,
+description text not null,
+city varchar(255) not null,
+country varchar(255) not null,
+expiration date not null,
+openings int(3) not null,
+posted date not null,
 primary key (id)
+);
+create table applicants
+(
+email varchar(255) not null,
+firstname varchar(255) not null,
+lastname varchar(255) not null,
+city varchar(255) not null,
+country varchar(255) not null,
+phone varchar(255) not null,
+primary key (email)
+);
+create table resumes
+(
+id int not null auto_increment,
+email varchar(255) not null,
+resume text not null,
+updated timestamp,
+primary key (id),
+foreign key (email) references applicants(email)
+);
+create table work_history
+(
+id int not null auto_increment,
+email varchar(255) not null,
+title varchar(255) not null,
+employer varchar(255) not null,
+start_date date not null,
+end_date date,
+primary key (id),
+foreign key (email) references applicants(email)
 );
 
 -- build relationships
